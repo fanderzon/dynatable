6eea0d46a750e340c6d3f6d98ad76c9a0a16098b
Fredrik Andersson <fredrik.andersson@widespace.com>
2017-03-22 18:16:46 +0100

    Added real tests for promise-wrapper

--- src/promise-wrapper.test.js
+++ src/promise-wrapper.test.js
@@ -1,10 +1,25 @@
 import promiseWrapper from './promise-wrapper';
 
+const pinkPony = {pink: 'pony'};
+const deadPony = new Error('dead pony');
 const someApi = {
-  methodA: () => {}
+  methodA: () => {},
+  methodB: (params, cb) => cb(null, pinkPony)
 };
 describe('promiseWrapper', () => {
   it('Should return a promise', () => {
     expect(promiseWrapper(someApi, 'methodA', {})).toBeInstanceOf(Promise);
+  });
+  it('Should resolve with callback value', () => {
+    promiseWrapper(someApi, 'methodB', {})
+      .then(data => expect(data).toEqual(pinkPony));
+  });
+  it('Should reject with callback error', () => {
+    promiseWrapper(someApi, 'methodB', {})
+      .catch(err => expect(err).toBe(deadPony))
+  });
+  it('Should reject with when calling undefined methods', () => {
+    promiseWrapper(someApi, 'methodZ', {})
+      .catch(err => expect(err).toBeDefined())
   });
 });
