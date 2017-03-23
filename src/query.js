9263503c4a582ab0f4b2adb821fd8306d8fb22ae
Fredrik Andersson <fredrik.andersson@widespace.com>
2017-03-23 20:22:21 +0100

    Added splitKeysAndParams tests for non object input

--- src/query.js
+++ src/query.js
@@ -1,8 +1,14 @@
+import { isObject } from './util';
+
 // Splits an object of query params into 2 objects
 // One for any params that matches the defined table keys
 // and another object for all other params
-// {id: 1, name: 'pony', interests: 'ponying'} -> [{id: 1}, {name: 'pony', interests: 'ponying'}]
+// ({id: 1, name: 'pony', interests: 'ponying'}, { id: 'N' }) -> [{id: 1}, {name: 'pony', interests: 'ponying'}]
 export function splitKeysAndParams(params, tableKeys) {
+  if (!isObject(params) || !isObject(tableKeys)) {
+    return null;
+  }
+
   const keys = Object.keys(tableKeys);
   const paramKeys = Object.keys(params);
   return [
--- src/query.test.js
+++ src/query.test.js
@@ -4,7 +4,15 @@ const paramsA = { id: 1, name: 'pink pony', interests: 'ponying?' };
 const keysA = { id: 'N' };
 
 describe('splitKeysAndParams', () => {
-  it('Should work', () => {
+  it('Should split out the keys into an object at index 0', () => {
     expect(splitKeysAndParams(paramsA, keysA)[0]).toEqual({id: 1});
+  });
+  it('Should split out the params into an object at index 1', () => {
+    expect(splitKeysAndParams(paramsA, keysA)[1]).toEqual({name: 'pink pony', interests: 'ponying?'});
+  });
+
+  it('Should return null on faulty input', () => {
+    expect(splitKeysAndParams('a string', keysA)).toEqual(null);
+    expect(splitKeysAndParams(paramsA, ['1', 2, 3])).toEqual(null);
   });
 });
