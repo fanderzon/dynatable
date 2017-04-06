8076aa54256bd3b1b48a719d4d6e369a927b52c0
Fredrik Andersson <fredrik.andersson@widespace.com>
2017-04-06 05:58:44 +0200

    Changed export of `find` to a named export, and included a new exported function `findOne` in the same module

--- src/find.js
+++ src/find.js
@@ -2,8 +2,7 @@ import promiseWrapper from './promise-wrapper';
 import { splitKeysAndParams, createFilterQuery } from './query';
 import { isObject } from './util';
 
-export default function find({ docClient, TableName, params, tableKeyDefinition }) {
-  console.log('find', TableName, tableKeyDefinition);
+export function find({ docClient, TableName, params, tableKeyDefinition }) {
   const [Key, scanParams] = splitKeysAndParams(params, tableKeyDefinition);
 
   // If we have any params that aren't `key` attributes in the table we
@@ -11,11 +10,30 @@ export default function find({ docClient, TableName, params, tableKeyDefinition 
   if (isObject(scanParams) && Object.keys(scanParams).length > 0) {
     return promiseWrapper(docClient, 'scan', Object.assign({
       TableName,
-    }, createFilterQuery(scanParams)));
+    }, createFilterQuery(scanParams)))
+      .then(handleFindResults);
   }
 
   return promiseWrapper(docClient, 'get', {
     TableName,
     Key,
-  });
+  })
+    .then(handleFindResults);
+}
+
+export function findOne({ docClient, TableName, params, tableKeyDefinition }) {
+  return find({ docClient, TableName, params, tableKeyDefinition })
+    .then(res => res && res[0] || null);
+}
+
+function handleFindResults(res) {
+  // Ensure that the response has either of the data properties, otherwise return an empty array
+  if (!res.Items && !res.Item) {
+    return [];
+  }
+
+  // Ensure that `.find()` always returns an array
+  // `.get()` will return an Item prop with a single object
+  // `.scan()` will return an Items prop with one or more objects
+  return res.Items ? res.Items : [ res.Item ];
 }
