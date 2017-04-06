e7b467c2b9441ded7862aef290a3c30f09ef5555
Fredrik Andersson <fredrik.andersson@widespace.com>
2017-04-06 06:38:29 +0200

    Added test to ensure that `get` is called for table keys and `scan` for other keys

--- src/find.test.js
+++ src/find.test.js
@@ -17,9 +17,20 @@ describe('find', () => {
 
   it('Should reject on callback errors', () => {
     docClient = {
-      get: jest.fn((params, cb) => cb('Something went wrong'))
+      get: jest.fn((params, cb) => cb('Something went wrong')),
     };
     find({ docClient, TableName, tableKeyDefinition, params })
       .catch(err => expect(err).toBeDefined());
+  });
+
+  it('Should call `get` for table keys and `scan` for other keys', () => {
+    docClient = {
+      get: jest.fn(),
+      scan: jest.fn(),
+    };
+    find({ docClient, TableName, tableKeyDefinition, params: { id: 5 } });
+    expect(docClient.get.mock.calls.length).toBe(1);
+    find({ docClient, TableName, tableKeyDefinition, params: { blorg: 5 } });
+    expect(docClient.scan.mock.calls.length).toBe(1);
   });
 });
